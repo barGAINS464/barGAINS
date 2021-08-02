4d349e1a99369f9bf3e947ee7fbf36af4fd00a19
Glen Larita <glarita@hawaii.edu>
2021-08-02 00:17:16 -1000

    Landing page formatting

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -9,7 +9,15 @@ import { Roles } from 'meteor/alanning:roles';
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
-    const menuStyle = { marginBottom: '10px' };
+    const menuStyle = {
+      marginBottom: '10px',
+      backgroundColor: '#ADD1D2',
+      border: 0,
+      borderRadius: 0,
+      margin: 0,
+      boxShadow: 'none',
+      position: 'sticky',
+      zIndex: 1 };
     return (
       <Menu style={menuStyle} attached="top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,33 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Button, Header, Grid, Image, Container } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
+      <div className='landingPage' id='landing-page'>
+        <Container textAlign='center'>
+          <Grid verticalAlign='middle' textAlign='center' container>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
+            <Grid.Column width={4}>
+              <Image size='small' circular src="/images/meteor-logo.png"/>
+            </Grid.Column>
 
-        <Grid.Column width={8}>
-          <h1>Welcome to barGAINS</h1>
-          <p>Now get to work and modify this app!</p>
-        </Grid.Column>
+            <Grid.Column width={8}>
+              <Header as='h1' inverted>Welcome to barGAINS</Header>
+              <Header as='h3' inverted>Where you can buy, sell, and exchange technology, books, and music!</Header>
+              <Container textAlign='center'>
+                <Button size="medium" as={Link} to='/Signup'>
+                  Get Started
+                </Button>
+              </Container>
+            </Grid.Column>
+          </Grid>
+        </Container>
 
-      </Grid>
+      </div>
     );
   }
 }
