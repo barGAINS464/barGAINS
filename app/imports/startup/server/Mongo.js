b9d0de0345a8636352b81b8f162f836bddc19844
alyssandra-cabading <alyssand@hawaii.edu>
2021-08-03 20:34:30 -1000

    MONGO added ProfileAccounts Collection data.

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
+import { ProfileAccounts } from '../../api/profile/ProfileAccount';
 
 /* eslint-disable no-console */
 
@@ -7,10 +8,19 @@ import { Stuffs } from '../../api/stuff/Stuff.js';
 function addData(data) {
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Stuffs.collection.insert(data);
+  ProfileAccounts.collection.insert(data);
 }
 
 // Initialize the StuffsCollection if empty.
 if (Stuffs.collection.find().count() === 0) {
+  if (Meteor.settings.defaultData) {
+    console.log('Creating default data.');
+    Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+// Initialize the ProfileAccountsCollection if empty.
+if (ProfileAccounts.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
