48e9a4f12e82802cc27b47bf5618d7d10d067cf8
alyssandra-cabading <alyssand@hawaii.edu>
2021-08-12 21:11:33 -1000

    images change

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Header, Grid, Container } from 'semantic-ui-react';
+import { Button, Header, Grid, Container, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 /** A simple static component to render some text for the landing page. */
@@ -14,9 +14,10 @@ class Landing extends React.Component {
         <Container textAlign='center'>
           <Grid verticalAlign='middle' textAlign='center' container>
             <Grid.Column width={9}>
-              <Header as='h1' inverted>Welcome to barGAINS</Header>
-              <Header as='h3' inverted>Where you can buy, sell, and exchange technology, books, and music!</Header>
-              <Header as='h2' inverted>No pay, no Gains!</Header>
+              <Image size='large' src="/images/bargains.png" centered/>
+              <Header as='h1' inverted >Welcome to barGAINS</Header>
+              <Header as='h3' inverted >Where you can buy, sell, and exchange technology, books, and music!</Header>
+              <Header as='h2' inverted >No pay, no Gains!</Header>
               <Container textAlign='center'>
                 <Button size="medium" as={Link} to='/Signup' style={landingButton}>
                   Get Started
